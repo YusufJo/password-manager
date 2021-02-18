6e79b17dd5fb20a535ea23e53b746a665c3b07e6
GoodbyeBlues <elizabeth.graham2000@gmail.com>
2021-02-18 16:02:23 -0600

    Initial Commit. Functional password generator

---
+++ generator.py
@@ -0,0 +1,9 @@
+import string
+import random
+
+def password_generator(length=8, upper=string.ascii_uppercase, lower=string.ascii_lowercase, digits=string.digits, punctuation=string.punctuation):
+    """ Generate Password according to user specifications
+        Arguments: Password length(int), A-Z(bool), a-z(bool), 0-9(bool), !@#$(bool)
+        :return:
+    """
+    return ''.join(random.SystemRandom().choice(upper + lower + digits + punctuation) for i in range(length))
